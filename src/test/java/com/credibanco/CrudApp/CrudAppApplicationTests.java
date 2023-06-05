c556ff669b4e8f9029601888b0d8734697e28cb0
juanAl95 <111659111+juanAl95@users.noreply.github.com>
2023-06-05 17:58:29 -0500

    Add files via upload

---
+++ src/test/java/com/credibanco/CrudApp/CrudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credibanco.CrudApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
